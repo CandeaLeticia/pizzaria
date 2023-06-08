44748cc083af9b0cb6a55bcda9fd7f4340e4daf3
CandeaLeticia <lele_candea@outlook.com>
2023-06-07 22:04:27 -0300

    Update Pizza.java

---
+++ aplicacao/App.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import model.Pizza;
+
+public class App {
+    public static void main(String[] args) {
+        Pizza pedido = new Pizza("Frango", 30, 'p');
+
+        System.out.println("Pizza criada!!!");
+        System.out.println(pedido.pedidoIfood());
+
+    }
+}
